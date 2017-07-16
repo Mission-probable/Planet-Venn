7abd04eaef3bf88685fc56116c9ebf0a1440ef23
Stuart Schafer <stuart_schafer@hotmail.com>
2017-07-16 14:13:30 -0400

    :hibiscus: Fixed issues with images not displaying

--- app/components/children/grandchildren/Footer.js
+++ app/components/children/grandchildren/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 	    return (
             <div id="footer">
 	    	    <p>Created by Project-Probable &#169; 2017</p>
-                <a href="https://github.com/Mission-probable/Planet-Venn"><img src="../../../public/images/github.png" alt="github logo" width="50"/></a>
+                <a href="https://github.com/Mission-probable/Planet-Venn"><img src="./images/github.png" alt="github logo" width="50"/></a>
             </div>
 	   	);
 };
--- app/components/children/grandchildren/Nav.js
+++ app/components/children/grandchildren/Nav.js
@@ -13,7 +13,7 @@ var Nav = React.createClass({
 	    	<nav className="navbar navbar-default">
                 <div className="container-fluid">
                     <div className="navbar-header">
-                        <h1><img src="../../../public/images/logo.png" alt="logo" width="50"/> Planet Venn</h1>
+                        <h1><img src="./images/logo.png" alt="logo" width="50"/> Planet Venn</h1>
                     </div>
                     <ul className="nav navbar-nav navbar-right">
 		    	        <li><Link to="/rules"><button className="btn btn-warning navbar-btn"> Rules </button> </Link></li>
