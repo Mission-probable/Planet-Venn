d81c465ef13650c87076e2bc98697d1ba8a4f62e
Mary Morvan <mamorvan@yahoo.com>
2017-07-16 19:36:39 -0400

    github image prettier :art:

--- app/components/children/grandchildren/Footer.js
+++ app/components/children/grandchildren/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 	    return (
             <div id="footer">
 	    	    <p>Created by Project-Probable &copy; 2017</p>
-                <a id="githug-img" href="https://github.com/Mission-probable/Planet-Venn"><img src="./images/github.png" alt="github logo" width="30"/></a>
+                <a href="https://github.com/Mission-probable/Planet-Venn"><img src="./images/github.png" alt="github logo" width="30"/></a>
             </div>
 	   	);
 };
