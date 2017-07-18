74401e4d83a7577545ed264e69771aebf1f162fe
Michael Eggers <mdeggers84@gmail.com>
2017-07-18 18:24:06 -0400

    adjusted spacing, edited layout

--- app/components/children/Game.js
+++ app/components/children/Game.js
@@ -1,39 +1,42 @@
 // dependency
-var React = require("react");
+var React = require('react');
 
 // components
-var Nav = require("./grandchildren/Nav");
-var Message = require("./grandchildren/Message");
-var Pieces = require("./grandchildren/Pieces");
-var Board = require("./grandchildren/Board");
-var Checklist = require("./grandchildren/Checklist");
-var Footer = require("./grandchildren/Footer");
-var Square = require("./grandchildren/Square");
+var Nav = require('./grandchildren/Nav');
+var Message = require('./grandchildren/Message');
+var Pieces = require('./grandchildren/Pieces');
+var Board = require('./grandchildren/Board');
+var Checklist = require('./grandchildren/Checklist');
+var Footer = require('./grandchildren/Footer');
+var Square = require('./grandchildren/Square');
 
 var Game = React.createClass({
-	render: function() {
-	    return (
-	    	<div className="container">
-		    	<Nav />
-                    <div id="message-container" className="row">
-		    	        <Message />
-                    </div>
-                    <div id="pieces-container" className="row">
-                        <div className="col-sm-8">
-				            <Pieces />
-							<Square />
-                        </div>
-                        <div id="board-container" className="col-sm-1">
-				            <Board />
-                        </div>
-                        <div id="checklist-container" className="col-sm-2">
-				            <Checklist />
-                        </div>
-                    </div>
-				<Footer />
-			</div>
-	   	);
-	}
+  render: function () {
+    return (
+      <div className="container">
+        <Nav />
+        <div id="message-container" className="row">
+          <Message />
+        </div>
+        <div id="pieces-container" className="row">
+
+          <div className="col-sm-2">
+            <Pieces />
+          </div>
+
+          <div id="board-container" className="col-sm-8">
+            <Board />
+          </div>
+
+          <div id="checklist-container" className="col-sm-2">
+            <Checklist />
+          </div>
+
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 });
 
 module.exports = Game;
