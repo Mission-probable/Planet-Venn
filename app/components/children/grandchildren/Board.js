1b3443ae364eda9f8edb88275e92e0ae42ed9a1d
Michael Eggers <mdeggers84@gmail.com>
2017-07-18 18:25:04 -0400

    moved pieces html to board component

--- app/components/children/grandchildren/Board.js
+++ app/components/children/grandchildren/Board.js
@@ -1,15 +1,29 @@
 // here we create the game board (venn diagram, rectangle for pieces that don't belong,
-//-- and rule choice drop down)
+// -- and rule choice drop down)
 
 // dependency
-var React = require("react");
+var React = require('react');
 
 var Board = React.createClass({
-    render: function() {
-	    return (
-	    	<p> This is where the Board might go and we can give it more room when needed</p>
-	   	);
-	}
+  render: function () {
+    return (
+      <div className="row">
+        <div className="col-sm-4">
+          <img id="category1" data-rule="TBD" alreadyPlaced="false" src="./images/emptybox.gif" />
+          <h1>NONE OF THE ABOVE</h1>
+          <img id="category4" alreadyPlaced="false" src="./images/emptybox.gif" />
+        </div>
+
+        <div className="col-sm-2">
+          <img id="category3" alreadyPlaced="false" src="./images/emptybox.gif" />
+        </div>
+
+        <div className="col-sm-2">
+          <img id="category2" data-rule="TBD" alreadyPlaced="false" src="./images/emptybox.gif" />
+        </div>
+      </div>
+    );
+  }
 
 });
 
--- app/components/children/grandchildren/Pieces.js
+++ app/components/children/grandchildren/Pieces.js
@@ -1,63 +1,48 @@
 // Include React
-import React from "react";
-var Link = require("react-router").Link;
+import React from 'react';
+var Link = require('react-router').Link;
 
 var Pieces = React.createClass({
 
   // Here we render the component
-		render: function() {
-				return (
-						<div className="row">
-								<div className="col-sm-3">
-										<div className="panel panel-primary">
-												<div className="panel-heading">
-														<h3 className="panel-title text-center">Playable Pieces</h3>
-														<button type="button" id="resetPieces" className="btn btn-danger btn-sm">Reset All Pieces</button>
-														<p>Correct places: <span id="right">0</span></p>
-														<p>Incorrect places: <span id="wrong">0</span></p>
-														<div className="panel-body">
-														
-																<img id="satBlueBig" className="big piece" data-shape="sat" data-color="blue" data-size="big" data-spot="false" data-placed="false" src="./images/sat.gif" />
-																<img id="alienGreenSmall" className="small piece" data-shape="alien" data-color="green" data-size="small" data-spot="false" data-placed="false" src="./images/alien.gif" />
-																<img id="sunRedBig" className="big piece" data-shape="sun" data-color="red" data-size="big" data-spot="false" data-placed="false" src="./images/sun.gif" />
-																<img id="sunBlueSmall" className="small piece" data-shape="sun" data-color="blue" data-size="small" data-placed="false" src="./images/sun.gif" />
+  render: function () {
+    return (
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="panel panel-primary">
+            <div className="panel-heading">
+              <h3 className="panel-title text-center">Playable Pieces</h3>
+              <button type="button" id="resetPieces" className="btn btn-danger btn-sm">Reset All Pieces</button>
+              <p>Correct places: <span id="right">0</span></p>
+              <p>Incorrect places: <span id="wrong">0</span></p>
+              <div className="panel-body">
 
-																<img id="alienGreenBig" className="big piece" data-shape="alien" data-color="green" data-size="big" data-placed="false" src="./images/alien.gif" />
-																<img id="satBlueSmall" className="small piece" data-shape="sat" data-color="blue" data-size="small" data-placed="false" src="./images/sat.gif" />
-																<img id="sunGreenBig" className="big piece" data-shape="sun" data-color="green" data-size="big" data-placed="false" src="./images/sun.gif" />
-																<img id="alienRedSmall" className="small piece" data-shape="alien" data-color="red" data-size="small" data-placed="false" src="./images/alien.gif" />
-															
-																<img id="alienBlueBig" className="big piece" data-shape="alien" data-color="blue" data-size="big" data-placed="false" src="./images/alien.gif" />
-																<img id="satGreenSmall" className="small piece" data-shape="sat" data-color="green" data-size="small" data-placed="false" src="./images/sat.gif" />
-																<img id="satRedBig" className="big piece" data-shape="sat" data-color="red" data-size="big" data-placed="false" src="./images/sat.gif" />
-																<img id="sunRedSmall" className="small piece" data-shape="sun" data-color="red" data-size="small" data-spot="false" data-placed="false" src="./images/sun.gif" />
+                <img id="satBlueBig" className="big piece" data-shape="sat" data-color="blue" data-size="big" data-spot="false" data-placed="false" src="./images/sat.gif" />
+                <img id="alienGreenSmall" className="small piece" data-shape="alien" data-color="green" data-size="small" data-spot="false" data-placed="false" src="./images/alien.gif" />
+                <img id="sunRedBig" className="big piece" data-shape="sun" data-color="red" data-size="big" data-spot="false" data-placed="false" src="./images/sun.gif" />
+                <img id="sunBlueSmall" className="small piece" data-shape="sun" data-color="blue" data-size="small" data-placed="false" src="./images/sun.gif" />
 
-														</div>
-												</div>
-										</div>
-								</div>
+                <img id="alienGreenBig" className="big piece" data-shape="alien" data-color="green" data-size="big" data-placed="false" src="./images/alien.gif" />
+                <img id="satBlueSmall" className="small piece" data-shape="sat" data-color="blue" data-size="small" data-placed="false" src="./images/sat.gif" />
+                <img id="sunGreenBig" className="big piece" data-shape="sun" data-color="green" data-size="big" data-placed="false" src="./images/sun.gif" />
+                <img id="alienRedSmall" className="small piece" data-shape="alien" data-color="red" data-size="small" data-placed="false" src="./images/alien.gif" />
 
-								<div className="col-sm-4">
-										
-										<img id="category1" data-rule="TBD" alreadyPlaced="false" src="./images/emptybox.gif" />
-										<h1>NONE OF THE ABOVE</h1>
-										<img id="category4" alreadyPlaced="false" src="./images/emptybox.gif" />
-								</div>
-								<div className="col-sm-2">
-									
-										<img id="category3" alreadyPlaced="false" src="./images/emptybox.gif" />
-								</div>
-								<div className="col-sm-2">
-									
-										<img id="category2" data-rule="TBD" alreadyPlaced="false" src="./images/emptybox.gif" />
-								</div>
+                <img id="alienBlueBig" className="big piece" data-shape="alien" data-color="blue" data-size="big" data-placed="false" src="./images/alien.gif" />
+                <img id="satGreenSmall" className="small piece" data-shape="sat" data-color="green" data-size="small" data-placed="false" src="./images/sat.gif" />
+                <img id="satRedBig" className="big piece" data-shape="sat" data-color="red" data-size="big" data-placed="false" src="./images/sat.gif" />
+                <img id="sunRedSmall" className="small piece" data-shape="sun" data-color="red" data-size="small" data-spot="false" data-placed="false" src="./images/sun.gif" />
 
-								{this.props.children}
+              </div>
+            </div>
+          </div>
+        </div>
 
-						</div>
+        {this.props.children}
 
-				);
-		}
+      </div>
+
+    );
+  }
 });
 
 // Export the component back for use in other files
