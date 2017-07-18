9bd51a3e043a52374d31db8ec3186e34127e4100
Mary Morvan <mamorvan@yahoo.com>
2017-07-18 10:27:19 -0400

    SignIn ajax request :snail:

--- app/components/children/SignIn.js
+++ app/components/children/SignIn.js
@@ -23,6 +23,25 @@ var SignIn = React.createClass({
     processForm: function(event) {
         event.preventDefault();
 
+        //string for HTTP body message
+        var email = encodeURIComponent(this.state.user.email);
+        var password = encodeURIComponent(this.state.user.password);
+        var formData = `email=${email}&password=${password}`;
+
+        //AJAX request (sadly no jQuery)
+        var xhr = new XMLHttpRequest();
+        xhr.open("post", "/auth/signin");
+        xhr.setRequestHeader("Content-type", "applicaton/x-www-form-urlencoded");
+        xhr.responseType = "json";
+        xhr.addEventListener("load", () => {
+            if (xhr.status === 200) {
+                console.log("SignIn.js ajax request valid");
+            } else {
+                console.log("SignIn.js ajax request problem- xhr.status: ", xhr.status);
+            }
+        });
+        xhr.send(formData);
+
         console.log("email: ", this.state.user.email);
         console.log("password: ", this.state.user.password);
     },
