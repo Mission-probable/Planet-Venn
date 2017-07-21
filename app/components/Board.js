8c281643c1ae0ec8ca52c63dc382ff21370ef336
Stuart Schafer <stuart_schafer@hotmail.com>
2017-07-21 01:46:14 -0400

    :crocodile: jquery now works

--- app/components/Board.js
+++ app/components/Board.js
@@ -9,25 +9,25 @@ var Board = React.createClass({
 
           <div className="col-sm-4">
             <h4 className="text-center">Category 1</h4>
-            <div className="well" id="category1" data-rule="TBD" alreadyPlaced="false" src="./images/emptybox.gif" />
+            <div className="welly" id="category1" data-rule="red" alreadyPlaced="false" />
           </div>
 
           <div className="col-sm-4">
             <h4 className="text-center">Both</h4>
-            <div className="well" id="category3" data-rule="TBD" alreadyPlaced="false" src="./images/emptybox.gif" />
+            <div className="welly" id="category3" data-rule="TBD" alreadyPlaced="false" />
           </div>
 
           <div className="col-sm-4">
             <h4 className="text-center">Category 2</h4>
-            <div className="well" id="category2" data-rule="TBD" alreadyPlaced="false" src="./images/emptybox.gif" />
+            <div className="welly" id="category2" data-rule="sun" alreadyPlaced="false" />
           </div>
 
         </div>
 
         <div className="row">
           <div className="col-sm-12">
+            <div className="welly" id="category4" data-rule="TBD" alreadyPlaced="false" />
             <h4 className="text-center">Does Not Belong</h4>
-            <div className="well" id="category4" data-rule="TBD" alreadyPlaced="false" src="./images/emptybox.gif" />
           </div>
         </div>
       </div>
