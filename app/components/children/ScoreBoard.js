28569e8afd8ee9beaf435389823eb68d4a75aad8
Mary Morvan <mamorvan@yahoo.com>
2017-07-27 13:10:33 -0400

    a little more styling :art:

--- app/components/children/ScoreBoard.js
+++ app/components/children/ScoreBoard.js
@@ -4,76 +4,63 @@ import { Row, Col } from 'react-grid-system';
 class ScoreBoard extends Component {
     render() {
         return (
-            <div id="scoreboard-container">
-                
-                    <h2>Top Scores</h2>
-                    <Row>
-                        <Col sm={6}>
-                            <h3>Player Name</h3>
-                        </Col>
-                        <Col sm={3}>
-                            <h3>Score</h3>
-                        </Col>
-                        <Col sm={3}>
-                            <h3>Date?</h3>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col sm={6}>
-                            <h3>Player Name</h3>
-                        </Col>
-                        <Col sm={3}>
-                            <h3>Score</h3>
-                        </Col>
-                        <Col sm={3}>
-                            <h3>Date?</h3>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col sm={6}>
-                            <h3>Player Name</h3>
-                        </Col>
-                        <Col sm={3}>
-                            <h3>Score</h3>
-                        </Col>
-                        <Col sm={3}>
-                            <h3>Date?</h3>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col sm={6}>
-                            <h3>Player Name</h3>
-                        </Col>
-                        <Col sm={3}>
-                            <h3>Score</h3>
-                        </Col>
-                        <Col sm={3}>
-                            <h3>Date?</h3>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col sm={6}>
-                            <h3>Player Name</h3>
-                        </Col>
-                        <Col sm={3}>
-                            <h3>Score</h3>
-                        </Col>
-                        <Col sm={3}>
-                            <h3>Date?</h3>
-                        </Col>
-                    </Row>   
-                    <Row>
-                        <Col sm={6}>
-                            <h3>Player Name</h3>
-                        </Col>
-                        <Col sm={3}>
-                            <h3>Score</h3>
-                        </Col>
-                        <Col sm={3}>
-                            <h3>Date?</h3>
-                        </Col>
-                    </Row>
-          
+            <div id="scoreboard-container"> 
+                <h2>TOP SCORES</h2>
+                <Row>
+                    <Col sm={6}>
+                        <h3>Player Name</h3>
+                    </Col>
+                    <Col sm={3}>
+                        <h3>Score</h3>
+                    </Col>
+                    <Col sm={3}>
+                        <h3>Date?</h3>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={6}>
+                        <h3>Player Name</h3>
+                    </Col>
+                    <Col sm={3}>
+                        <h3>Score</h3>
+                    </Col>
+                    <Col sm={3}>
+                        <h3>Date?</h3>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={6}>
+                        <h3>Player Name</h3>
+                    </Col>
+                    <Col sm={3}>
+                        <h3>Score</h3>
+                    </Col>
+                    <Col sm={3}>
+                        <h3>Date?</h3>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={6}>
+                        <h3>Player Name</h3>
+                    </Col>
+                    <Col sm={3}>
+                        <h3>Score</h3>
+                    </Col>
+                    <Col sm={3}>
+                        <h3>Date?</h3>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={6}>
+                        <h3>Player Name</h3>
+                    </Col>
+                    <Col sm={3}>
+                        <h3>Score</h3>
+                    </Col>
+                    <Col sm={3}>
+                        <h3>Date?</h3>
+                    </Col>
+                </Row>   
             </div>
         )
     }
